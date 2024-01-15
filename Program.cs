8f749e359445642fb1b4595b0ff27156101f2854
Jeff Lord <jeff@jefflord.com>
2024-01-15 08:28:18 -0500

    Refactor Win32 stuff

--- Program.cs
+++ Program.cs
@@ -20,97 +20,7 @@ namespace HelloPhotinoApp
             public int y { get; set; }
         }
 
-        const int HWND_TOPMOST = -1;
-        const int SW_HIDE = 0;
-        const int SW_SHOWNORMAL = 1;
-        const int SW_SHOW = 5;
-        const int SWP_NOSIZE = 0x0001;
-        const int SWP_NOMOVE = 0x0002;
-        const int SWP_NOZORDER = 0x0004;
-        const int SWP_SHOWWINDOW = 0x0040;
-        const int HORZRES = 8;
-        const int VERTRES = 10;
 
-
-        const int WM_SYSCOMMAND = 0x0112;
-        const int SC_MINIMIZE = 0xF020;
-
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct WindowMessage
-        {
-            public IntPtr hWnd;
-            public uint msg;
-            public IntPtr wParam;
-            public IntPtr lParam;
-            public uint time;
-            public Point p;
-        }
-
-        static readonly IntPtr HWND_TOP = IntPtr.Zero;
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
-            public int Left;
-            public int Top;
-            public int Right;
-            public int Bottom;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        struct MONITORINFO
-        {
-            public int cbSize;
-            public RECT rcMonitor;
-            public RECT rcWork;
-            public uint dwFlags;
-        }
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
-
-        [DllImport("user32.dll")]
-        private static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
-
-        [DllImport("user32.dll")]
-        static extern IntPtr GetDesktopWindow();
-
-        [DllImport("user32.dll")]
-        static extern IntPtr GetWindowDC(IntPtr hWnd);
-
-        [DllImport("gdi32.dll")]
-        static extern int GetDeviceCaps(IntPtr hDC, int nIndex);
-
-        [DllImport("user32.dll")]
-        static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
-
-        [DllImport("user32.dll")]
-        static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
-
-        private const uint WINEVENT_OUTOFCONTEXT = 0;
-        private const uint EVENT_SYSTEM_MINIMIZESTART = 0x0016;
-
-        // Delegate for the hook callback
-        private delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
 
 
         [STAThread]
@@ -313,11 +223,11 @@ namespace HelloPhotinoApp
             Process process = Process.GetCurrentProcess();
             IntPtr mainWindowHandle = process.MainWindowHandle;
             {
-                MONITORINFO monitorInfo = new MONITORINFO();
+                var monitorInfo = new Win32.MONITORINFO();
                 monitorInfo.cbSize = Marshal.SizeOf(monitorInfo);
 
-                IntPtr monitor = MonitorFromWindow(mainWindowHandle, 0);
-                GetMonitorInfo(monitor, ref monitorInfo);
+                IntPtr monitor = Win32.MonitorFromWindow(mainWindowHandle, 0);
+                Win32.GetMonitorInfo(monitor, ref monitorInfo);
 
                 Console.WriteLine($"Current Screen: {monitorInfo.rcMonitor}");
             }
@@ -327,16 +237,15 @@ namespace HelloPhotinoApp
 
 
 
-            RECT windowRect;
-            GetWindowRect(mainWindowHandle, out windowRect);
+            Win32.RECT windowRect;
+            Win32.GetWindowRect(mainWindowHandle, out windowRect);
 
             int currentX = windowRect.Left;
             int currentY = windowRect.Top;
 
             int newX = currentX - 1;
             int newY = currentY;
-
-            SetWindowPos(mainWindowHandle, HWND_TOP, newX, newY, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            Win32.SetWindowPos(mainWindowHandle, Win32.HWND_TOP, newX, newY, 0, 0, Win32.SWP_NOSIZE | Win32.SWP_NOZORDER);
             SaveWindowLocation(newX, newY);
         }
 
@@ -353,7 +262,7 @@ namespace HelloPhotinoApp
             var config = LoadConfig();
             Process process = Process.GetCurrentProcess();
             IntPtr mainWindowHandle = process.MainWindowHandle;
-            SetWindowPos(mainWindowHandle, HWND_TOP, config.x, config.y + 0, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            Win32.SetWindowPos(mainWindowHandle, Win32.HWND_TOP, config.x, config.y + 0, 0, 0, Win32.SWP_NOSIZE | Win32.SWP_NOZORDER);
 
         }
 
@@ -380,16 +289,16 @@ namespace HelloPhotinoApp
         {
             Process process = Process.GetCurrentProcess();
             IntPtr mainWindowHandle = process.MainWindowHandle;
-            RECT windowRect;
-            GetWindowRect(mainWindowHandle, out windowRect);
+            Win32.RECT windowRect;
+            Win32.GetWindowRect(mainWindowHandle, out windowRect);
 
             int currentX = windowRect.Left;
             int currentY = windowRect.Top;
 
             int newX = currentX + 1;
             int newY = currentY;
-
-            SetWindowPos(mainWindowHandle, HWND_TOP, newX, newY, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            Win32.
+                        SetWindowPos(mainWindowHandle, Win32.HWND_TOP, newX, newY, 0, 0, Win32.SWP_NOSIZE | Win32.SWP_NOZORDER);
             SaveWindowLocation(newX, newY);
         }
 
@@ -397,16 +306,16 @@ namespace HelloPhotinoApp
         {
             Process process = Process.GetCurrentProcess();
             IntPtr mainWindowHandle = process.MainWindowHandle;
-            RECT windowRect;
-            GetWindowRect(mainWindowHandle, out windowRect);
+            Win32.RECT windowRect;
+            Win32.GetWindowRect(mainWindowHandle, out windowRect);
 
             int currentX = windowRect.Left;
             int currentY = windowRect.Top;
 
             int newX = currentX;
             int newY = currentY - 1;
-
-            SetWindowPos(mainWindowHandle, HWND_TOP, newX, newY, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            Win32.
+                        SetWindowPos(mainWindowHandle, Win32.HWND_TOP, newX, newY, 0, 0, Win32.SWP_NOSIZE | Win32.SWP_NOZORDER);
             SaveWindowLocation(newX, newY);
         }
 
@@ -416,23 +325,23 @@ namespace HelloPhotinoApp
         {
             Process process = Process.GetCurrentProcess();
             IntPtr mainWindowHandle = process.MainWindowHandle;
-            RECT windowRect;
-            GetWindowRect(mainWindowHandle, out windowRect);
+            Win32.RECT windowRect;
+            Win32.GetWindowRect(mainWindowHandle, out windowRect);
 
             int currentX = windowRect.Left;
             int currentY = windowRect.Top;
 
             int newX = currentX;
             int newY = currentY + 1;
-
-            SetWindowPos(mainWindowHandle, HWND_TOP, newX, newY, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            Win32.
+                        SetWindowPos(mainWindowHandle, Win32.HWND_TOP, newX, newY, 0, 0, Win32.SWP_NOSIZE | Win32.SWP_NOZORDER);
         }
 
 
         private static void ShowYTM()
         {
-            SetForegroundWindow(windowHandle);
-            ShowWindow(windowHandle, SW_SHOW);
+            Win32.SetForegroundWindow(windowHandle);
+            Win32.ShowWindow(windowHandle, Win32.SW_SHOW);
         }
 
         private static void FindYTM()
@@ -473,16 +382,17 @@ namespace HelloPhotinoApp
 
                     if (windowHandle != IntPtr.Zero)
                     {
+                        Win32.
 
+                                                SetForegroundWindow(windowHandle);
+                        Win32.
+                                                //SetWindowPos(windowHandle, IntPtr.Zero, -10000, -10000, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
 
-                        SetForegroundWindow(windowHandle);
-                        //SetWindowPos(windowHandle, IntPtr.Zero, -10000, -10000, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+                                                //ShowWindow(windowHandle, SW_SHOWNORMAL);
 
-                        //ShowWindow(windowHandle, SW_SHOWNORMAL);
+                                                //SetWindowPos(windowHandle, IntPtr.Zero, -10000, -10000, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
 
-                        //SetWindowPos(windowHandle, IntPtr.Zero, -10000, -10000, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-
-                        ShowWindow(windowHandle, SW_HIDE);
+                                                ShowWindow(windowHandle, Win32.SW_HIDE);
 
                         // Bring the window to the foreground
 
@@ -512,7 +422,7 @@ namespace HelloPhotinoApp
                 IntPtr h = pList.MainWindowHandle;
                 process = pList;
                 StringBuilder windowText = new StringBuilder(256);
-                GetWindowText(h, windowText, 256);
+                Win32.GetWindowText(h, windowText, 256);
 
                 if (!string.IsNullOrEmpty(windowText.ToString()))
                 {
---
+++ Win32.cs
@@ -0,0 +1,102 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace HelloPhotinoApp
+{
+    internal static class Win32
+    {
+
+        internal const int HWND_TOPMOST = -1;
+        internal const int SW_HIDE = 0;
+        internal const int SW_SHOWNORMAL = 1;
+        internal const int SW_SHOW = 5;
+        internal const int SWP_NOSIZE = 0x0001;
+        internal const int SWP_NOMOVE = 0x0002;
+        internal const int SWP_NOZORDER = 0x0004;
+        internal const int SWP_SHOWWINDOW = 0x0040;
+        internal const int HORZRES = 8;
+        internal const int VERTRES = 10;
+
+
+        internal const int WM_SYSCOMMAND = 0x0112;
+        internal const int SC_MINIMIZE = 0xF020;
+
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_MINIMIZESTART = 0x0016;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct WindowMessage
+        {
+            public IntPtr hWnd;
+            public uint msg;
+            public IntPtr wParam;
+            public IntPtr lParam;
+            public uint time;
+            public Point p;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct MONITORINFO
+        {
+            public int cbSize;
+            public RECT rcMonitor;
+            public RECT rcWork;
+            public uint dwFlags;
+        }
+
+        internal static readonly IntPtr HWND_TOP = IntPtr.Zero;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDesktopWindow();
+
+        [DllImport("gdi32.dll")]
+        static extern int GetDeviceCaps(IntPtr hDC, int nIndex);
+
+        [DllImport("user32.dll")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
+
+        [DllImport("user32.dll")]
+        internal static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        internal static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll")]
+        internal static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        // Delegate for the hook callback
+        internal delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+    }
+}
